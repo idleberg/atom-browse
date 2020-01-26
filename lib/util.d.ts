dd57aabc87aa8b51837f0dbc57124f7918397eb7
idleberg <git@idleberg.com>
2020-01-26 16:49:42 +0100

    yarn build

--- lib/util.d.ts
+++ lib/util.d.ts
@@ -1,6 +1,9 @@
+/// <reference types="node" />
+import { stat } from 'fs';
+declare const statAsync: typeof stat.__promisify__;
 declare const getConfig: (key?: string) => any;
 declare const getPackagesDirs: () => string[];
 declare const showFolder: (folderName: string, filePath: string) => Promise<void>;
 declare const showInFolder: (filePath: string) => Promise<void>;
 declare const warn: (message: string, dismissable?: boolean) => void;
-export { getConfig, getPackagesDirs, showFolder, showInFolder, warn };
+export { getConfig, getPackagesDirs, showFolder, showInFolder, statAsync as stat, warn };
