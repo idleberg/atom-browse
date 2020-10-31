59c832bb2f535d6f3df4dfb51f485764dc76f322
idleberg <git@idleberg.com>
2020-10-31 15:16:53 +0100

    add fallback for missing action property

--- src/commands/service.ts
+++ src/commands/service.ts
@@ -1,9 +1,11 @@
 import { basename } from 'path';
-import { isFile, showFolder, showInFolder } from '../util';
+import { isDirectory, isFile, showFolder, showInFolder } from '../util';
 import * as console from '@atxm/developer-console';
 
 async function browseService(payload: BrowseServicePayload): Promise<void> {
   if (typeof payload === 'string') {
+    console.log('[browse] Auto-handle string input');
+
     payload = {
       action: await isFile(payload)
         ? 'reveal'
@@ -21,9 +23,9 @@ async function browseService(payload: BrowseServicePayload): Promise<void> {
       console.warn(`[browse] Skipping: target path is of type ${typeof targetPath}, should be string`);
     }
 
-    if (payload.action === 'reveal') {
+    if (payload.action === 'reveal' || await isFile(targetPath)) {
       showInFolder(targetPath)
-    } else if (payload.action === 'open') {
+    } else if (payload.action === 'open' || await isDirectory(targetPath)) {
       showFolder(basename(targetPath), targetPath);
     } else if (payload.action?.length) {
       console.warn(`[browse] Action '${payload.action}' is not supported`);
