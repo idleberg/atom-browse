b851894a3183de5f7b1f74fbdb2e05d72224495c
Jan T. Sott <git@idleberg.com>
2024-02-01 00:08:47 +0100

    add Pulsar support to appDataFolder

--- src/commands/app-data.ts
+++ src/commands/app-data.ts
@@ -1,21 +1,26 @@
+import { getAppName } from '../util';
+
 async function appDataFolder(): Promise<void> {
   const { homedir, platform } = await import('os');
   const { resolve } = await import('path');
   const { showFolder } = await import('../util');
 
+  const appName = getAppName();
   let appDataFolder: string;
 
   switch (platform()) {
     case 'darwin':
-      appDataFolder = resolve(homedir(), 'Library', 'Application Support', 'Atom');
+      appDataFolder = resolve(homedir(), 'Library', 'Application Support', appName);
       break;
     case 'win32':
+
       appDataFolder = (process.env.APPDATA)
-        ? resolve(process.env.APPDATA, 'Atom')
+        ? resolve(process.env.APPDATA, appName)
         : '';
       break;
+
     default:
-      appDataFolder = resolve(homedir(), '.config', 'Atom');
+      appDataFolder = resolve(homedir(), '.config', appName);
       break;
   }
 
