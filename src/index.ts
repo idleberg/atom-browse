280b98737ce1e28e528a964e5cfaef36eacd7fb4
Jan T. Sott <git@idleberg.com>
2024-09-29 10:25:50 +0200

    fix linting errors

--- src/index.ts
+++ src/index.ts
@@ -4,113 +4,113 @@ import console from './log';
 import configSchema from './config';
 
 export default {
-  config: configSchema,
-  subscriptions: new CompositeDisposable(),
+	config: configSchema,
+	subscriptions: new CompositeDisposable(),
 
-  async activate(): Promise<void> {
-    console.log('Activating package');
+	async activate(): Promise<void> {
+		console.log('Activating package');
 
-    // Register commands
-    this.subscriptions.add(
-      atom.commands.add('atom-workspace', {
-        'browse:.apm-folder': async () => {
-          const { apmFolder } = await import('./commands/apm');
-          await apmFolder();
-        }
-      })
-    );
+		// Register commands
+		this.subscriptions.add(
+			atom.commands.add('atom-workspace', {
+				'browse:.apm-folder': async () => {
+					const { apmFolder } = await import('./commands/apm');
+					await apmFolder();
+				},
+			}),
+		);
 
-    this.subscriptions.add(
-      atom.commands.add('atom-workspace', {
-        'browse:app-data-folder': async () => {
-          const { appDataFolder } = await import('./commands/app-data');
-          await appDataFolder();
-        }
-      })
-    );
+		this.subscriptions.add(
+			atom.commands.add('atom-workspace', {
+				'browse:app-data-folder': async () => {
+					const { appDataFolder } = await import('./commands/app-data');
+					await appDataFolder();
+				},
+			}),
+		);
 
-    this.subscriptions.add(
-      atom.commands.add('atom-workspace', {
-        'browse:application-folder': async () => {
-          const { appFolder } = await import('./commands/application');
-          await appFolder();
-        }
-      })
-    );
+		this.subscriptions.add(
+			atom.commands.add('atom-workspace', {
+				'browse:application-folder': async () => {
+					const { appFolder } = await import('./commands/application');
+					await appFolder();
+				},
+			}),
+		);
 
-    this.subscriptions.add(
-      atom.commands.add('atom-workspace', {
-        'browse:configuration-folder': async () => {
-          const { configFolder } = await import('./commands/configuration');
-          await configFolder()
-        }
-      })
-    );
+		this.subscriptions.add(
+			atom.commands.add('atom-workspace', {
+				'browse:configuration-folder': async () => {
+					const { configFolder } = await import('./commands/configuration');
+					await configFolder();
+				},
+			}),
+		);
 
-    this.subscriptions.add(
-      atom.commands.add('atom-workspace', {
-        'browse:packages-folder': async () => {
-          const { packagesFolder } = await import('./commands/packages');
-          await packagesFolder();
-        }
-      })
-    );
+		this.subscriptions.add(
+			atom.commands.add('atom-workspace', {
+				'browse:packages-folder': async () => {
+					const { packagesFolder } = await import('./commands/packages');
+					await packagesFolder();
+				},
+			}),
+		);
 
-    this.subscriptions.add(
-      atom.commands.add('atom-workspace', {
-        'browse:project-folders': async () => {
-          const { projectFolders } = await import('./commands/project');
-          projectFolders();
-        }
-      })
-    );
+		this.subscriptions.add(
+			atom.commands.add('atom-workspace', {
+				'browse:project-folders': async () => {
+					const { projectFolders } = await import('./commands/project');
+					projectFolders();
+				},
+			}),
+		);
 
-    this.subscriptions.add(
-      atom.commands.add('atom-workspace', {
-        'browse:project-dependencies': async () => {
-          const { projectDependencies } = await import('./commands/dependencies');
-          projectDependencies();
-        }
-      })
-    );
+		this.subscriptions.add(
+			atom.commands.add('atom-workspace', {
+				'browse:project-dependencies': async () => {
+					const { projectDependencies } = await import('./commands/dependencies');
+					projectDependencies();
+				},
+			}),
+		);
 
-    this.subscriptions.add(
-      atom.commands.add('atom-workspace', {
-        'browse:resources-folder': async () => {
-          const { resourcesFolder } = await import('./commands/resources');
-          resourcesFolder();
-        }
-      })
-    );
+		this.subscriptions.add(
+			atom.commands.add('atom-workspace', {
+				'browse:resources-folder': async () => {
+					const { resourcesFolder } = await import('./commands/resources');
+					resourcesFolder();
+				},
+			}),
+		);
 
-    this.subscriptions.add(
-      atom.commands.add('atom-workspace', {
-        'browse:reveal-open-files': async () => {
-          const { revealFiles } = await import('./commands/reveal');
-          revealFiles();
-        }
-      })
-    );
+		this.subscriptions.add(
+			atom.commands.add('atom-workspace', {
+				'browse:reveal-open-files': async () => {
+					const { revealFiles } = await import('./commands/reveal');
+					revealFiles();
+				},
+			}),
+		);
 
-    this.subscriptions.add(
-      atom.commands.add('atom-workspace', {
-        'browse:reveal-file': async () => {
-          const { revealFile } = await import('./commands/reveal');
-          revealFile();
-        }
-      })
-    );
-  },
+		this.subscriptions.add(
+			atom.commands.add('atom-workspace', {
+				'browse:reveal-file': async () => {
+					const { revealFile } = await import('./commands/reveal');
+					revealFile();
+				},
+			}),
+		);
+	},
 
-  deactivate(): void {
-    console.log('Deactivating package');
+	deactivate(): void {
+		console.log('Deactivating package');
 
-    this.subscriptions && this.subscriptions.dispose();
-  },
+		this.subscriptions?.dispose();
+	},
 
-  provideBrowse(): unknown {
-    console.log('Providing service');
+	provideBrowse(): unknown {
+		console.log('Providing service');
 
-    return browseService;
-  }
-}
+		return browseService;
+	},
+};
--- src/util.ts
+++ src/util.ts
@@ -10,216 +10,193 @@ import { name } from '../package.json';
 const spawnAsync = promisify(spawn);
 
 interface ShowOptions {
-  message?: string;
-  name?: string;
-  path: string;
-  silent?: boolean;
+	message?: string;
+	name?: string;
+	path: string;
+	silent?: boolean;
 }
 
 async function fileExists(pathName: string): Promise<boolean> {
-  try {
-    await fs.access(pathName, constants.F_OK);
-  } catch (error) {
-    console.warn(`Skipping '${pathName}' – not found`);
+	try {
+		await fs.access(pathName, constants.F_OK);
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	} catch (error) {
+		console.warn(`Skipping '${pathName}' – not found`);
 
-    return false;
-  }
+		return false;
+	}
 
-  return true;
+	return true;
 }
 
 async function isDirectory(pathName: string): Promise<boolean> {
-  let stats;
+	let stats;
 
-  try {
-    stats = await fs.lstat(pathName);
-  } catch (error) {
-    console.error(error);
+	try {
+		stats = await fs.lstat(pathName);
+	} catch (error) {
+		console.error(error);
 
-    return false;
-  }
+		return false;
+	}
 
-  return stats.isDirectory();
+	return stats.isDirectory();
 }
 
 async function isFile(pathName: string): Promise<boolean> {
-  let stats;
+	let stats;
 
-  try {
-    stats = await fs.lstat(pathName);
-  } catch (error) {
-    console.error(error);
+	try {
+		stats = await fs.lstat(pathName);
+	} catch (error) {
+		console.error(error);
 
-    return false;
-  }
+		return false;
+	}
 
-  return stats.isFile();
+	return stats.isFile();
 }
 
 async function folderExists(pathName: string): Promise<boolean> {
-  let stats;
+	let stats;
 
-  try {
-    stats = await fs.stat(pathName);
-  } catch (error) {
-    console.warn(`Skipping '${pathName}' – not found`);
+	try {
+		stats = await fs.stat(pathName);
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	} catch (error) {
+		console.warn(`Skipping '${pathName}' – not found`);
 
-    return false;
-  }
+		return false;
+	}
 
-  return stats.isDirectory();
+	return stats.isDirectory();
 }
 
 const getAppName = (): string => {
-  return atom.getAppName().split(' ')[0];
-}
+	return atom.getAppName().split(' ')[0];
+};
 
 const getConfig = (key = ''): any => {
-  return atom.config.get(`browse.${key}`);
-}
+	return atom.config.get(`browse.${key}`);
+};
 
 const getPackagesDirs = (): string[] => {
-  const packageDirs: string[] = atom.packages.getPackageDirPaths();
+	const packageDirs: string[] = atom.packages.getPackageDirPaths();
 
-  return packageDirs.filter( (val: string) => !val.includes('app.asar') );
-}
+	return packageDirs.filter((val: string) => !val.includes('app.asar'));
+};
 
 const getFileManager = (): string => {
-  switch (platform()) {
-    case 'darwin':
-      return 'Finder';
+	switch (platform()) {
+		case 'darwin':
+			return 'Finder';
 
-    case 'win32':
-      return 'Explorer';
+		case 'win32':
+			return 'Explorer';
 
-    default:
-      return 'file manager';
-  }
-}
+		default:
+			return 'file manager';
+	}
+};
 
 async function showFolder(options: ShowOptions | string): Promise<void> {
-  const filePath = typeof options === 'string'
-    ? options
-    : options.path;
+	const filePath = typeof options === 'string' ? options : options.path;
 
-  if (!filePath.length || !await folderExists(filePath)) return;
+	if (!filePath.length || !(await folderExists(filePath))) return;
 
-  const fileManager = getConfig('customFileManager.fullPath');
+	const fileManager = getConfig('customFileManager.fullPath');
 
-  if (fileManager) {
-    let openArgs = getConfig('customFileManager.openArgs');
+	if (fileManager) {
+		let openArgs = getConfig('customFileManager.openArgs');
 
-    if (openArgs.length > 0) {
-      if (openArgs.includes('%path%')) {
-        const index = openArgs.indexOf('%path%');
+		if (openArgs.length > 0) {
+			if (openArgs.includes('%path%')) {
+				const index = openArgs.indexOf('%path%');
 
-        openArgs[index] = openArgs[index].replace('%path%', filePath);
-      } else {
-        openArgs.push(filePath);
-      }
-    } else {
-      openArgs = [ filePath ];
-    }
+				openArgs[index] = openArgs[index].replace('%path%', filePath);
+			} else {
+				openArgs.push(filePath);
+			}
+		} else {
+			openArgs = [filePath];
+		}
 
-    if (typeof options !== 'string' && !options?.silent && options?.message?.length && options?.name?.length) {
-      info(options.message
-        ? String(options.message)
-        : `Opening '${options.name}' in custom file manager`
-      );
-    }
+		if (typeof options !== 'string' && !options?.silent && options?.message?.length && options?.name?.length) {
+			info(options.message ? String(options.message) : `Opening '${options.name}' in custom file manager`);
+		}
 
-    spawnAsync(fileManager, openArgs, {});
-  } else {
-    if (typeof options !== 'string' && !options?.silent && options?.message?.length && options?.name?.length) {
-      info(options.message
-        ? String(options.message)
-        : `Opening '${options.name}' in ${getFileManager()}`
-      );
-    }
+		spawnAsync(fileManager, openArgs, {});
+	} else {
+		if (typeof options !== 'string' && !options?.silent && options?.message?.length && options?.name?.length) {
+			info(options.message ? String(options.message) : `Opening '${options.name}' in ${getFileManager()}`);
+		}
 
-    try {
-      await shell.openPath(filePath);
-    } catch (err) {
-      // Electron <9
-      if (shell['openItem']) shell['openItem'](filePath);
-    }
-  }
+		try {
+			await shell.openPath(filePath);
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		} catch (error) {
+			// Electron <9
+			if (shell['openItem']) shell['openItem'](filePath);
+		}
+	}
 }
 
 async function showInFolder(options: ShowOptions | string): Promise<void> {
-  const filePath = typeof options === 'string'
-    ? options
-    : options.path;
+	const filePath = typeof options === 'string' ? options : options.path;
 
-  if (!filePath.length || !(await fileExists(filePath))) return;
+	if (!filePath.length || !(await fileExists(filePath))) return;
 
-  const fileManager = getConfig('customFileManager.fullPath');
+	const fileManager = getConfig('customFileManager.fullPath');
 
-  if (fileManager) {
-    let revealArgs = getConfig('customFileManager.revealArgs');
+	if (fileManager) {
+		let revealArgs = getConfig('customFileManager.revealArgs');
 
-    if (revealArgs.length > 0) {
-      if (revealArgs.includes('%path%')) {
-        const index = revealArgs.indexOf('%path%');
+		if (revealArgs.length > 0) {
+			if (revealArgs.includes('%path%')) {
+				const index = revealArgs.indexOf('%path%');
 
-        revealArgs[index] = revealArgs[index].replace('%path%', filePath);
-      } else {
-        revealArgs.push(filePath);
-      }
-    } else {
-      revealArgs = [ filePath ];
-    }
+				revealArgs[index] = revealArgs[index].replace('%path%', filePath);
+			} else {
+				revealArgs.push(filePath);
+			}
+		} else {
+			revealArgs = [filePath];
+		}
 
-    if (typeof options !== 'string' && !options?.silent && options?.message?.length) {
-      info(options.message
-        ? String(options.message)
-        : `Revealing \`${basename(filePath)}\` in custom file manager`
-      );
-    }
+		if (typeof options !== 'string' && !options?.silent && options?.message?.length) {
+			info(options.message ? String(options.message) : `Revealing \`${basename(filePath)}\` in custom file manager`);
+		}
 
-    spawnAsync(fileManager, revealArgs, {});
-  } else {
-    if (typeof options !== 'string' && !options?.silent && options?.message?.length) {
-      info(options.message
-        ? String(options.message)
-        : `Revealing \`${basename(filePath)}\` in ${getFileManager()}`
-      );
-    }
+		spawnAsync(fileManager, revealArgs, {});
+	} else {
+		if (typeof options !== 'string' && !options?.silent && options?.message?.length) {
+			info(options.message ? String(options.message) : `Revealing \`${basename(filePath)}\` in ${getFileManager()}`);
+		}
 
-    shell.showItemInFolder(filePath);
-  }
+		shell.showItemInFolder(filePath);
+	}
 }
 
 function info(message: string, dismissable = false): void {
-  if (getConfig('notify') === 'all') {
-    atom.notifications.addInfo(`**${name}** ${message}`, {
-      dismissable: dismissable,
-      icon: 'check'
-    });
-  }
+	if (getConfig('notify') === 'all') {
+		atom.notifications.addInfo(`**${name}** ${message}`, {
+			dismissable: dismissable,
+			icon: 'check',
+		});
+	}
 
-  console.info(`${message}`);
+	console.info(`${message}`);
 }
 
 function warn(message: string, dismissable = false): void {
-  if (getConfig('notify') !== 'none') {
-    atom.notifications.addWarning(`**${name}** ${message}`, {
-      dismissable: dismissable
-    });
-  }
+	if (getConfig('notify') !== 'none') {
+		atom.notifications.addWarning(`**${name}** ${message}`, {
+			dismissable: dismissable,
+		});
+	}
 
-  if (getConfig('beep')) atom.beep();
-  console.warn(`${message}`);
+	if (getConfig('beep')) atom.beep();
+	console.warn(`${message}`);
 }
 
-export {
-  folderExists,
-  getAppName,
-  getConfig,
-  getPackagesDirs,
-  isDirectory,
-  isFile,
-  showFolder,
-  showInFolder,
-  warn
-};
+export { folderExists, getAppName, getConfig, getPackagesDirs, isDirectory, isFile, showFolder, showInFolder, warn };
